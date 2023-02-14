14d787befe6b85b2de50c6f93c1e2e916c70e09e
hskr313 <hadi.prom@gmail.com>
2023-02-14 09:52:51 +0100

    adding get methods in RentalController

--- src/main/java/com/example/carlocation/controllers/RentalController.java
+++ src/main/java/com/example/carlocation/controllers/RentalController.java
@@ -1,14 +1,40 @@
 package com.example.carlocation.controllers;
 
+import com.example.carlocation.exceptions.HttpNotFoundException;
+import com.example.carlocation.models.dtos.rental.RentalDTO;
+import com.example.carlocation.models.entities.Rental;
 import com.example.carlocation.services.rental.RentalService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
-public class RentalController {
+@RequestMapping(path = "/rental")
+public class RentalController implements BaseRestController<RentalDTO, Long> {
 
     private final RentalService rentalService;
 
     public RentalController(RentalService rentalService) {
         this.rentalService = rentalService;
+    }
+
+    @GetMapping(path = "/{id:[0_9]+}")
+    public ResponseEntity<RentalDTO> readOne(@PathVariable Long id) {
+
+        Rental rental = this.rentalService.readOneByKey(id).orElseThrow( () -> new HttpNotFoundException("Rental with id : " + id + "does not exist"));
+
+        return ResponseEntity.ok(RentalDTO.toDTO(rental)) ;
+    }
+
+    @GetMapping(path = "")
+    public ResponseEntity<Collection<RentalDTO>> readAll() {
+
+        return ResponseEntity.ok(this.rentalService.readAll()
+                .map(RentalDTO::toDTO)
+                .toList());
     }
 }
---
+++ src/main/java/com/example/carlocation/jobs/PrintReservation.java
@@ -0,0 +1,7 @@
+package com.example.carlocation.jobs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrintReservation {
+}
